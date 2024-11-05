5b085887b0dc8439e1b32ea0c9b7aacb3261b8fd
Lucas Bonggio <lucasbonggio1903@gmail.com>
2024-11-05 17:38:59 -0300

    main.py

--- main.py
+++ main.py
@@ -1,53 +1,35 @@
+
 from functs import *
-from encript import generar_key, cargar_key
+from encript import *
 import sys
 
 def main():
-    # Verifica si existe el archivo de clave, si no, genera una nueva clave
     try:
         cargar_key()
     except FileNotFoundError:
         generar_key()
 
     while True:
-        print('QUE TAREA DESEA REALIZAR? \n A. Generar nueva cuenta \n B. Ver cuentas \n C. Eliminar cuenta \n D. Actualizar cuenta \n E. Salir')
-        resp = input('Seleccione una tarea.')
-        if resp.upper() == 'A': 
-            sitio = input('¿Dónde vas a guardar esta contraseña? ')
-            email = input('¿Y el email? ')
-            contraseña = generate_pass(10) 
-            # Crear la base de datos y la tabla si no existen
-            crear_database()    
+        print("¿Qué tarea desea realizar?")
+        print("A. Generar nueva cuenta \n B. Ver cuentas \n C. Eliminar cuenta \n D.Actualizar contraseña \n E. Salir")
+        resp = input("Seleccione una tarea. ")
+        if resp.upper() == "A":
+            crear_database()
             crear_tabla()
-            # Insertar la cuenta en la base de datos
-            insertar_cuenta(sitio, email, contraseña)
+            guardar_cuenta()
         elif resp.upper() == 'B':
             ver_cuentas()
-            option = input('Desencriptar las contraseñas? \n (SI/NO) \n ')
-            if option.upper() == 'SI':
-                ver_cuentas_desencriptadas()
-            else:
-                pass
         elif resp.upper() == 'C':
-            sitio = input('De que sitio es la cuenta a eliminar?')
-            eliminar_cuenta(sitio)
-
+            cuenta = input('¿Qué cuenta desea eliminar?')
+            eliminar_cuenta(cuenta)
         elif resp.upper() == 'D':
-            resp = input('Desea cambiar una contraseña o email? ')
-            if resp.lower() == 'contraseña':
-                sitio = input('De que sitio es la cuenta? ')
-                nueva_contraseña = generate_pass(10)
-                actualizar_contraseña(sitio, nueva_contraseña)
-                print('La contraseña se modificó correctamente.')
-            else:
-                sitio = input('De que sitio es la cuenta? ')
-                actualizar_cuenta(resp, sitio)
-                print('El email se modificó correctamente.')
-        else:
+            cuenta_actualizar = input('¿Qué cuenta desea actualizar?')
+            actualizar_cuenta(cuenta_actualizar)
+        elif resp.upper() == 'E':
             print('Saliendo del programa...')
             sys.exit()
-            
-
+        else:
+            print('Elija una opción válida.')
 
 if __name__ == '__main__':
     main()
