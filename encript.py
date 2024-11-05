72236614963ff2d385dea405b2bac4fdcdff4a0c
Lucas Bonggio <lucasbonggio1903@gmail.com>
2024-11-05 17:39:55 -0300

    encript.py

--- encript.py
+++ encript.py
@@ -1,45 +1,26 @@
 from cryptography.fernet import Fernet
 import base64
-
-# Generar clave y guardarla en un archivo
 def generar_key():
-    key = Fernet.generate_key()
-    with open('clave.key', 'wb') as key_file:
-        key_file.write(key)
+    key = Fernet.generate_key() #Generamos una clave
+    with open('clave.key', 'wb') as key_file:   #La abrimos en modo lectura binaria
+        key_file.write(key) #Escribimos la key en el archivo
 
-# Cargar clave desde un archivo
 def cargar_key():
     return open('clave.key', 'rb').read()
 
-# Encriptar datos
 def encriptar_datos(datos):
     key = cargar_key()
     f = Fernet(key)
-    encrypted_data = f.encrypt(datos.encode())
-    return base64.urlsafe_b64encode(encrypted_data).decode()  # Convertir a cadena
+    encriptar = f.encrypt(datos.encode())
+    return encriptar.decode()
 
-# Encriptar
-def encriptar(func):
-    def wrapper(sitio, email, contraseña):
-        contraseña_encriptada = encriptar_datos(contraseña)
-        return func(sitio, email, contraseña_encriptada)
-    return wrapper
-
-# Enctriptar solo contraseña
-def encriptar_new(func):
-    def wrapper(sitio, nueva_contraseña):
-        contraseña_encriptada = encriptar_datos(nueva_contraseña)
-        return func(sitio, contraseña_encriptada)
-    return wrapper
-
-# Desencriptar contraseña
-def desencriptar_contraseña(contraseña_encriptada):
+# Función para desencriptar datos
+def desencriptar_datos(contraseña_encriptada):
     try:
         key = cargar_key()
         f = Fernet(key)
-        # Convertir de cadena a bytes
-        encrypted_data = base64.urlsafe_b64decode(contraseña_encriptada)  
-        contraseña_desencriptada = f.decrypt(encrypted_data).decode()
+        # Aquí no es necesario usar base64.urlsafe_b64decode
+        contraseña_desencriptada = f.decrypt(contraseña_encriptada.encode()).decode()
         return contraseña_desencriptada
     except Exception as e:
         print("Error al desencriptar la contraseña:", e)
